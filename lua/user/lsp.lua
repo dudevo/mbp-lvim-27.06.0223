9f93626c28f18ab0556e3ac09ea1d75390db547c
dudevo <renpro1@me.com>
2023-08-06 16:52:34 +0200

    update config 06.08.23

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,3 +1,33 @@
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.view.width = 40
+
+local lspconfig = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+lspconfig.cssmodules_ls.setup {
+  filetypes = { "typescriptreact", "javascriptreact" },
+  init_options = {
+    camelCase = 'dashes',
+  },
+}
+
+lspconfig.emmet_ls.setup({
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "css", "html", "javascript", "javascriptreact", "scss", "typescriptreact" },
+  init_options = {
+    html = {
+      options = {
+        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+        ["bem.enabled"] = true,
+      },
+    },
+  }
+})
+
+lspconfig.angularls.setup({})
+
+
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup({
   {
@@ -5,8 +35,8 @@ formatters.setup({
     filetypes = {
       -- "javascript",
       -- "javascriptreact",
-      -- "typescript",
-      -- "typescriptreact",
+      "typescript",
+      "typescriptreact",
       "css",
       "scss",
       -- "html",
@@ -16,11 +46,3 @@ formatters.setup({
     }
   },
 })
-
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup({
---   {
---     command = "eslint_d",
---     filetypes = { "javascript", "typescript", "typescriptreact", "json" }
---   },
--- })
---
+++ lua/user/plugins.lua
@@ -0,0 +1,45 @@
+lvim.plugins = {
+  {
+    "pocco81/auto-save.nvim",
+  },
+  { "windwp/nvim-ts-autotag", event = "VeryLazy" },
+  { "mg979/vim-visual-multi" },
+  {
+    'Wansmer/treesj',
+    keys = { '<space>m', '<space>j', '<space>s' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('treesj').setup({ max_join_length = 1000, })
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    opts = { use_diagnostic_signs = true },
+    keys = {
+      { "<leader>xx", false },
+      { "<leader>xX", false },
+      { "<leader>xL", false },
+      { "<leader>xQ", false },
+      { "<leader>dx", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics (Trouble)" },
+      { "<leader>dX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+      { "<leader>dL", "<cmd>TroubleToggle loclist<cr>",               desc = "Location List (Trouble)" },
+      { "<leader>dQ", "<cmd>TroubleToggle quickfix<cr>",              desc = "Quickfix List (Trouble)" },
+    },
+  },
+  {
+    "codota/tabnine-nvim",
+    build = "./dl_binaries.sh",
+
+    config = function()
+      require('tabnine').setup({
+        disable_auto_comment = true,
+        accept_keymap = "<C-t>", -- "<TAB>"
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt" },
+        log_file_path = nil, -- absolute path to Tabnine log file
+      })
+    end
+  },
+}
